40ff15b9f1ce7e1f1be149a580de699d7e50f675
pablovem <pablovema@gmail.com>
2016-04-06 23:57:19 +0100

    Setup admin generation

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+  domain: "",
+  port: 3300,
+  admin : {
+    username : "",
+    email: "",
+    password : ""
+  }
+};
+
+module.exports = config;
--- routes/main.js
+++ routes/main.js
@@ -1,17 +1,14 @@
 /* Dependencies */
+var config = require('../config');
+
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
 router.get('/', function (req, res, next) {
   res.render('index', { user: req.user });
-});
-
-router.get('/admin/register', function (req, res, next) {
-  if(!req.user) {
-    res.redirect('/');
-  } else {
-    res.render('register', { user: req.user });
-  }
 });
 
 router.get('/resources', function (req, res, next) {
@@ -48,6 +45,31 @@ router.get('/profile', function (req, res, next) {
   } else {
     res.render('profile', { user : req.user });
   }
+});
+
+// Admin
+router.get('/admin/register', function (req, res, next) {
+  if(!req.user) {
+    res.redirect('/');
+  } else {
+    res.render('register', { user: req.user });
+  }
+});
+
+router.get('/admin/setup', function (req,res,next) {
+  var admin = new User({
+    username: config.admin.username,
+    email:    config.admin.email,
+    role:     "Admin"
+  });
+  //console.log(admin);
+
+  User.register(admin, config.admin.password, function(err, admin) {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect('/api/users');
+  });
 });
 
 module.exports = router;
